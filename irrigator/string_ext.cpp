f256073e50f253be06b59048ce29cec8ee5f16d1
Tamas Lustyik <elveestei@gmail.com>
2017-07-07 23:24:00 +0200

    Base64 decoder implementation

--- irrigator/string_ext.cpp
+++ irrigator/string_ext.cpp
@@ -31,3 +31,33 @@ String bisect(const String& src, const String& separator, String& tail) {
 
     return head;
 }
+
+bool base64Decode(const String& src, String& dst) {
+    if (src.length() % 4 != 0) {
+        return false;
+    }
+
+    static const String alphabet = F("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=");
+    char buf[src.length()*3 / 4 + 1];
+
+    int j = 0;
+    int b[4];
+
+    for (int i = 0; i < src.length(); i += 4) {
+        b[0] = alphabet.indexOf(src[i]);
+        b[1] = alphabet.indexOf(src[i + 1]);
+        b[2] = alphabet.indexOf(src[i + 2]);
+        b[3] = alphabet.indexOf(src[i + 3]);
+        buf[j++] = (char) ((b[0] << 2) | (b[1] >> 4));
+        if (b[2] < 64) {
+            buf[j++] = (char) ((b[1] << 4) | (b[2] >> 2));
+            if (b[3] < 64)  {
+                buf[j++] = (char) ((b[2] << 6) | b[3]);
+            }
+        }
+    }
+    buf[j] = 0;
+    dst = buf;
+
+    return true;
+}
